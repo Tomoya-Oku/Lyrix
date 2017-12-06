fa5fbdf1b5e1cca8b6a82c4163375cf1fe043bce
Oktm <comet.rice.m@gmail.com>
2017-12-06 23:22:57 +0900

    First Commit.
    Add all files.

---
+++ Lyrix/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Lyrix
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.iTunesと連携ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.設定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.ヘルプToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.button1 = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+			this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+			this.dataGridView1.Location = new System.Drawing.Point(0, 24);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.RowHeadersVisible = false;
+			this.dataGridView1.RowTemplate.Height = 21;
+			this.dataGridView1.Size = new System.Drawing.Size(624, 300);
+			this.dataGridView1.TabIndex = 0;
+			this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+			// 
+			// Column1
+			// 
+			this.Column1.HeaderText = "曲名";
+			this.Column1.Name = "Column1";
+			this.Column1.ReadOnly = true;
+			this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+			this.Column1.Width = 54;
+			// 
+			// Column2
+			// 
+			this.Column2.HeaderText = "アーティスト";
+			this.Column2.Name = "Column2";
+			this.Column2.ReadOnly = true;
+			this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+			this.Column2.Width = 82;
+			// 
+			// Column3
+			// 
+			this.Column3.HeaderText = "アルバム";
+			this.Column3.Name = "Column3";
+			this.Column3.ReadOnly = true;
+			this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+			this.Column3.Width = 69;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iTunesと連携ToolStripMenuItem,
+            this.設定ToolStripMenuItem,
+            this.ヘルプToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(624, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// iTunesと連携ToolStripMenuItem
+			// 
+			this.iTunesと連携ToolStripMenuItem.Name = "iTunesと連携ToolStripMenuItem";
+			this.iTunesと連携ToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+			this.iTunesと連携ToolStripMenuItem.Text = "iTunesと連携";
+			this.iTunesと連携ToolStripMenuItem.Click += new System.EventHandler(this.iTunesと連携ToolStripMenuItem_Click);
+			// 
+			// 設定ToolStripMenuItem
+			// 
+			this.設定ToolStripMenuItem.Name = "設定ToolStripMenuItem";
+			this.設定ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+			this.設定ToolStripMenuItem.Text = "設定";
+			// 
+			// ヘルプToolStripMenuItem
+			// 
+			this.ヘルプToolStripMenuItem.Name = "ヘルプToolStripMenuItem";
+			this.ヘルプToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+			this.ヘルプToolStripMenuItem.Text = "ヘルプ";
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.button1.Location = new System.Drawing.Point(12, 348);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(294, 54);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "START";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.richTextBox1.Location = new System.Drawing.Point(312, 330);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.ReadOnly = true;
+			this.richTextBox1.Size = new System.Drawing.Size(300, 99);
+			this.richTextBox1.TabIndex = 4;
+			this.richTextBox1.Text = "";
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 333);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(58, 12);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "すべての曲:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 405);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(296, 24);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "曲を選択した状態でSTARTボタンを押すと自動で歌詞を適用\r\nし始めます。色がついている曲は既に歌詞がある曲です。\r\n";
+			// 
+			// label3
+			// 
+			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(76, 333);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(23, 12);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "0曲";
+			// 
+			// label4
+			// 
+			this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(170, 333);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(63, 12);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "選択中の曲";
+			// 
+			// label5
+			// 
+			this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(239, 333);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(23, 12);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "0曲";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(624, 441);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.menuStrip1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MinimumSize = new System.Drawing.Size(640, 480);
+			this.Name = "Form1";
+			this.Text = "Lyrix";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem iTunesと連携ToolStripMenuItem;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.ToolStripMenuItem 設定ToolStripMenuItem;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ToolStripMenuItem ヘルプToolStripMenuItem;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+	}
+}
+
---
+++ Lyrix/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using mp3Lyric;
+using iTunesLib;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace Lyrix
+{
+	public partial class Form1 : Form
+	{
+		#region "列の項目"
+
+		private const int title = 0;
+		private const int artist = 1;
+		private const int album = 2;
+
+		#endregion
+
+		private Color BACKCOLOR = Color.SkyBlue; //既に歌詞がある場合の背景色
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void LeaveLog(string message)
+		{
+			richTextBox1.AppendText("[" + DateTime.Now.ToLongTimeString() + "] " + message + "\r\n");
+		}
+
+		private void iTunesと連携ToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			LeaveLog("iTunesの曲を取得しています。");
+			LeaveLog("これにはしばらく時間がかかることがあります。");
+
+			iTunesApp app;
+
+			try
+			{
+				app = new iTunesApp();
+			}
+			catch
+			{
+				MessageBox.Show("iTunesの起動に失敗しました。\nインストールされているか確認してください。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			IEnumerable<IITFileOrCDTrack> tracks = null;
+
+			try
+			{
+				tracks = app.LibraryPlaylist.Tracks.Cast<IITFileOrCDTrack>().OrderBy(n => n.Name).Cast<IITFileOrCDTrack>(); ;
+			}	
+			catch (ArgumentNullException)
+			{
+				MessageBox.Show("曲が見つかりませんでした。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			catch (InvalidCastException ex)
+			{
+				MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			var i = 0;
+			var error = 0;
+			foreach (var t in tracks)
+			{
+				try
+				{
+					dataGridView1.Rows.Add();
+					dataGridView1.Rows[i].Tag = t;
+					dataGridView1.Rows[i].Cells[title].Value = t.Name;
+					dataGridView1.Rows[i].Cells[artist].Value = t.Artist;
+					dataGridView1.Rows[i].Cells[album].Value = t.Album;
+				}
+				catch
+				{
+					error++;
+					continue;
+				}
+
+				//既に歌詞がある場合背景色を変更
+				if (t.Lyrics != null)
+				{
+					dataGridView1.Rows[i].Cells[0].Style.BackColor = BACKCOLOR;
+				}
+
+				i++;
+			}
+
+			label3.Text = tracks.Count() + "曲";
+
+			LeaveLog("iTunesの曲の取得が終了しました。");
+
+			if (error != 0)
+			{
+				LeaveLog(error + "曲の取得に失敗しました。");
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (dataGridView1.SelectedRows == null)
+			{
+				MessageBox.Show("曲が選択されていません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			IEnumerable<DataGridViewRow> selectedRows = null;
+
+			try
+			{
+				selectedRows = dataGridView1.SelectedRows.Cast<DataGridViewRow>();
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			foreach (var row in selectedRows)
+			{
+				if (string.IsNullOrWhiteSpace(row.Cells[1].Value.ToString()) | string.IsNullOrWhiteSpace(row.Cells[0].Value.ToString()))
+				{
+					LeaveLog("Failed! => ???/???");
+					continue;
+				}
+
+				var artist = "";
+				var title = "";
+
+				try
+				{
+					artist = row.Cells[1].Value.ToString();
+					title = row.Cells[0].Value.ToString();
+				}
+				catch
+				{
+					LeaveLog("Failed! => " + title + "/" + artist);
+					continue;
+				}
+				
+				var lg = new LyricGetter(artist, title);
+				var lyric = lg.GetLyric().TrimEnd(); //最後の空白文字を削除
+
+				if (lyric == null)
+				{
+					LeaveLog("Failed! => " + title + "/" + artist);
+					continue;
+				}
+				else
+				{
+					var track = (IITFileOrCDTrack)row.Tag;
+
+					try
+					{
+						track.Lyrics = lyric;
+					}
+					catch
+					{
+						LeaveLog("Failed! => " + title + "/" + artist);
+						continue;
+					}
+					
+					LeaveLog("Success! => " + title + "/" + artist);
+				}
+			}
+		}
+
+		private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+		{
+			IEnumerable<DataGridViewCell> cells;
+			
+			try
+			{
+				cells = dataGridView1.SelectedCells.Cast<DataGridViewCell>();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			foreach (var c in cells)
+			{
+				try
+				{
+					dataGridView1.Rows[c.RowIndex].Selected = true;
+					label5.Text = dataGridView1.SelectedRows.Count + "曲";
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					return;
+				}
+			}
+		}
+	}
+}
